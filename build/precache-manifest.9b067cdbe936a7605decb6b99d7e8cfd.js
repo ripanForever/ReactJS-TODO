07a9dfb5c98bd75efd7d547f194955038d30f0a5
Ripan Seal <41423026+ripanForever@users.noreply.github.com>
2021-11-30 10:23:37 +0530

    Add files via upload

---
+++ build/precache-manifest.9b067cdbe936a7605decb6b99d7e8cfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885fb8f49cf2244cb2c62abe36745acd",
+    "url": "/ReactJS-TODO/index.html"
+  },
+  {
+    "revision": "778fa96c63a68a30a59c",
+    "url": "/ReactJS-TODO/static/css/main.1231689e.chunk.css"
+  },
+  {
+    "revision": "07e703f07fdc371eb47d",
+    "url": "/ReactJS-TODO/static/js/2.d6ef7250.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS-TODO/static/js/2.d6ef7250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778fa96c63a68a30a59c",
+    "url": "/ReactJS-TODO/static/js/main.175712df.chunk.js"
+  },
+  {
+    "revision": "ec44213f32d38b9f46c6",
+    "url": "/ReactJS-TODO/static/js/runtime-main.2cb554f9.js"
+  },
+  {
+    "revision": "93326a916609381db6a9b85ae83f3f7f",
+    "url": "/ReactJS-TODO/static/media/LCO-logo-white.93326a91.png"
+  }
+]);
